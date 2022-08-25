85373504ce7765f6a3fb0c4485643a588a47f761
Wenn911 <wenn123@yandex.ru>
2022-08-25 18:48:46 +0300

    init proj and create basic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react';
+import Navigation from "./components/Navigation/Navigation";
+import ICard from "./components/Card/Card";
+import {Container, Row} from "react-bootstrap";
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="">
+          <Navigation />
+          <Container className='w-50'>
+              <Row>
+              <ICard title={'YUMMS GOTHIC LOGO TEE in BLACK'} price={'2200 ₽'} />
+              </Row>
+          </Container>
+      </div>
   );
 }
 
